d94d9ab1efa9effa2bbed107429f5070bbc9385b
whitesices <1642816705@qq.com>
2024-08-10 13:12:50 +0800

    first commit

---
+++ Source/TestNiagara/TestNiagaraGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestNiagaraGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestNiagaraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestNiagaraGameMode();
+};
+
+
+
